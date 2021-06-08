b5d66494640c5c6235884b40a9f45a6d86a7beb5
kanchanpriya <priyakanchan05@gmail.com>
2021-06-08 22:31:11 +0530

    edit

--- src/Common.jsx
+++ src/Common.jsx
@@ -11,7 +11,7 @@ return(
         <div className="row">
             <div className="col-10 mx-auto">
                 <div className="row">
-                <div className="col-6 pt-5 pt-lg-0 order-2 order-lg-1 d-flex justify-content-center flex-column">
+                <div className="col-md-6 pt-5 pt-lg-0 order-2 order-lg-1 d-flex justify-content-center flex-column">
                     <h1>{props.name} <strong className="brand-name">Best Developers</strong></h1>
                     <h2 className="my-3">
                         we are the team of talented developer making websites
